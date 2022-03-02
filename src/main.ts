964881496d57d68a2b30e04c534b4b049b6f11ff
tjehdgur1500 <tjehdgur1500@naver.com>
2022-03-03 01:02:49 +0900

    env 파일 노출 ignore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function serverInit() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3001);
 }
-bootstrap();
+serverInit();
