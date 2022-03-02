2b9cbc74e1314f6e6b515d83880cd52e9f82c85c
tjehdgur1500 <tjehdgur1500@naver.com>
2022-03-03 00:58:09 +0900

    database config 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection, getConnectionOptions } from 'typeorm';
+import { Connection } from 'typeorm';
 import { UserModule } from './user/user.module';
+import { DataBaseService } from './database/database.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({
-    useFactory: async () =>
-      Object.assign(await getConnectionOptions(), {
-        autoLoadEntities: true,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
       }),
-  }),
+    }),
+    TypeOrmModule.forRoot(
+      new DataBaseService(new ConfigService()).getTypeOrmConfig(),
+    ),
     UserModule,
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/database/database.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DataBaseService {
+    constructor(private readonly config: ConfigService) { }
+
+    public getTypeOrmConfig(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: +this.config.get('DATABASE_PORT'),
+            username: this.config.get('DATABASE_USER'),
+            password: this.config.get('DATABASE_PASSWORD'),
+            database: this.config.get('DATABASE_NAME'),
+            entities: ["dist/**/*.entity{.ts,.js}"],
+            synchronize: true,
+            autoLoadEntities: true,
+        };
+    }
+}
