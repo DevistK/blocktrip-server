4efeab1d050fa7b44a9dd4a41ad9f50c0d7f133a
tjehdgur1500 <tjehdgur1500@naver.com>
2022-05-01 18:03:43 +0900

    req 커스텀 데코레이터로 변경예정

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateUserDto } from '../dto/create-user.dto';
+import { CreateUserDto } from '../dto/create.user.dto';
 import { User } from '../user.entity';
 import { UserService } from '../services/user.service';
 
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -7,7 +7,7 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { hashPassword } from 'src/utils/bcrypt';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from '../dto/create-user.dto';
+import { CreateUserDto } from '../dto/create.user.dto';
 import { User } from '../user.entity';
 
 @Injectable()
